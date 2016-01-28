699147856660dbfb13e78fe32f665980f978abbe
Alexander <alexander.bertilsson98@gmail.com>
2016-01-28 10:44:42 +0100

    214213

--- Spelprojekt2k16/Assets/Movement.cs
+++ Spelprojekt2k16/Assets/Movement.cs
@@ -14,6 +14,11 @@ public class Movement : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        Vector2 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+
         //GetComponent<Rigidbody2D>().AddForce(Vector2.right * Input.GetAxis("Horizontal") * speed);
         //GetComponent<Rigidbody2D>().AddForce(Vector2.up * Input.GetAxis("Vertical") * speed);
         GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
